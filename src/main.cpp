6984b2bb41b599bb6a98939b7617281da60f2a78
Godley <charlotte.godley@hotmail.co.uk>
2016-01-15 00:15:24 +0000

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,117 @@
+#include <FastLED.h>
+
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+#include <Wire.h> // Must include Wire library for I2C
+#include <SFE_MMA8452Q.h> // Includes the SFE_MMA8452Q library
+
+#define PIN 8
+// How many leds in your strip?
+#define NUM_LEDS 30
+
+// For led chips like Neopixels, which have a data line, ground, and power, you just
+// need to define DATA_PIN.  For led chipsets that are SPI based (four wires - data, clock,
+// ground, and power), like the LPD8806 define both DATA_PIN and CLOCK_PIN
+#define DATA_PIN 4
+
+// Define the array of leds
+CRGB leds[NUM_LEDS];
+// Parameter 1 = number of pixels in strip
+// Parameter 2 = Arduino pin number (most are valid)
+// Parameter 3 = pixel type flags, add together as needed:
+//   NEO_KHZ800  800 KHz bitstream (most NeoPixel products w/WS2812 LEDs)
+//   NEO_KHZ400  400 KHz (classic 'v1' (not v2) FLORA pixels, WS2811 drivers)
+//   NEO_GRB     Pixels are wired for GRB bitstream (most NeoPixel products)
+//   NEO_RGB     Pixels are wired for RGB bitstream (v1 FLORA pixels, not v2)
+//   NEO_RGBW    Pixels are wired for RGBW bitstream (NeoPixel RGBW products)
+
+// IMPORTANT: To reduce NeoPixel burnout risk, add 1000 uF capacitor across
+// pixel power leads, add 300 - 500 Ohm resistor on first pixel's data input
+// and minimize distance between Arduino and first pixel.  Avoid connecting
+// on a live circuit...if you must, connect GND first.
+MMA8452Q accel; // Default MMA8452Q object create. (Address = 0x1D)
+
+void setup() {
+  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  //accel.init();
+  FastLED.addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);
+  // End of trinket special code
+
+
+
+}
+
+void printAccels()
+{
+  Serial.print(accel.x, 3);
+  Serial.print("\t");
+  Serial.print(accel.y, 3);
+  Serial.print("\t");
+  Serial.print(accel.z, 3);
+  Serial.print("\t");
+}
+
+// This function demonstrates how to use the accel.cx, accel.cy,
+//  and accel.cz variables.
+// Before using these variables you must call the accel.read()
+//  function!
+void printCalculatedAccels()
+{
+  Serial.print(accel.cx, 3);
+  Serial.print("\t");
+  Serial.print(accel.cy, 3);
+  Serial.print("\t");
+  Serial.print(accel.cz, 3);
+  Serial.print("\t");
+}
+
+// This function demonstrates how to use the accel.readPL()
+// function, which reads the portrait/landscape status of the
+// sensor.
+void printOrientation()
+{
+  // accel.readPL() will return a byte containing information
+  // about the orientation of the sensor. It will be either
+  // PORTRAIT_U, PORTRAIT_D, LANDSCAPE_R, LANDSCAPE_L, or
+  // LOCKOUT.
+  byte pl = accel.readPL();
+  switch (pl)
+  {
+  case PORTRAIT_U:
+    Serial.print("Portrait Up");
+    break;
+  case PORTRAIT_D:
+    Serial.print("Portrait Down");
+    break;
+  case LANDSCAPE_R:
+    Serial.print("Landscape Right");
+    break;
+  case LANDSCAPE_L:
+    Serial.print("Landscape Left");
+    break;
+  case LOCKOUT:
+    Serial.print("Flat");
+    break;
+  }
+}
+
+
+// Input a value 0 to 255 to get a color value.
+// The colours are a transition r - g - b - back to r.
+
+
+void loop() {
+  leds[0] = CRGB::Red;
+  FastLED.show();
+  delay(500);
+  // Now turn the LED off, then pause
+  leds[0] = CRGB::Black;
+  FastLED.show();
+  delay(500);
+}
